b92654f05455103c5b5206f3a075a938542f3b35
Ashish-Kumar-tandon <ashishkumartandon2002@gmail.com>
2025-02-07 11:03:45 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import WebcamCapture from './Webcam'; 
+import logo from "./logo.svg";
+import "./App.css";
+import WebcamCapture from "./Webcam";
+import Studentlist from "./Studentlist";
 
 function App() {
   return (
     <div className="App">
-      <WebcamCapture/>
+      <WebcamCapture />
+      <Studentlist />
     </div>
   );
 }
