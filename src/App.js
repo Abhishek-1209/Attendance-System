9c7ce0edf4f2e94a2e8c9193c784c4397f779ecc
Abhishek Kumar <abhishek@Abhisheks-MacBook-Air.local>
2025-04-19 13:48:38 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,10 +4,19 @@ import WebcamCapture from "./Webcam";
 import Studentlist from "./Studentlist";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-
+import { ToastContainer } from "react-toastify";
 function App() {
   return (
     <div className="App">
+      <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          closeOnClick
+          theme="light"
+          pauseOnHover={false}
+          draggable={false}
+        />
       <Header/>
       <WebcamCapture />
       <Studentlist />
--- src/Webcam/index.js
+++ src/Webcam/index.js
@@ -156,7 +156,7 @@ const WebcamCapture = () => {
       }
     }
   };
-
+  
   return (
     <div className="webcam">
       <Webcam
